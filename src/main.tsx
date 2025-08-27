555818e92f094fb1a2d5f02275eea42bfc92faf1
Edwin Gutierrez <edwin.gutierrez@bmw.com.mx>
2025-08-27 12:33:59 -0600

    fix: localize BMW Density Styles CSS and update import for Amplify build

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import '@bmw-ds/density-styles/dist/css/density-styles-full.css';
+import './assets/css/density-styles-full.css';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
